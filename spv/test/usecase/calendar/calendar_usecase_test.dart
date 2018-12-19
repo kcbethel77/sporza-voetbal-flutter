167e7fc200769e1fc4a65254fbad832f77526908
Tim Rijckaert <tim.rijckaert@hotmail.com>
2018-12-19 20:00:22 +0100

    calendar use case test

--- spv/test/usecase/calendar/calendar_usecase_test.dart
+++ spv/test/usecase/calendar/calendar_usecase_test.dart
@@ -1,30 +1,36 @@
 import 'package:rxdart/rxdart.dart';
+import 'package:spv/datasource/data_source_type.dart';
 import 'package:spv/models/network/network_models.dart';
+import 'package:spv/models/response.dart';
 import 'package:spv/usecase/use_case.dart';
 import 'package:test/test.dart';
 import 'package:mockito/mockito.dart';
 
 import '../../utils/spv_test_utils.dart';
 
+const _competitionId = "43";
+
 void main() {
   MockCache cache = MockCache();
-  when(cache.getT<Competition>(any)).thenAnswer((_) => Observable.empty());
+  when(cache.getT<Competition>(argThat(
+          equals(CalendarForCompetitionDataSourceType(_competitionId)))))
+      .thenAnswer((_) => Observable.empty());
 
   MockSporzaSoccerDataSource network = MockSporzaSoccerDataSource();
 
-  CalendarForCompetitionUseCase calendarForCompetitionUseCase = CalendarForCompetitionUseCase("42", cache, network);
+  CalendarForCompetitionUseCase calendarForCompetitionUseCase =
+      CalendarForCompetitionUseCase(_competitionId, cache, network);
 
   group("calendar for competition use case", () {
     test("simple test", () {
-      when(network.getT<Competition>(any)).thenAnswer((_) => Observable.just(buildCompetition()));
+      Competition competition = buildCompetition();
+      when(network.getT<Competition>(any))
+          .thenAnswer((_) => Observable.just(competition));
 
-      calendarForCompetitionUseCase.calendarForCompetition.listen(expectAsync1((resp) {
-        expect(resp, isNull);
+      calendarForCompetitionUseCase.calendarForCompetition
+          .listen(expectAsync1((resp) {
+        expect((resp as Data<Competition>).value, competition);
       }));
-
-      //calendarForCompetitionUseCase.calendarForCompetition.listen(expectAsync1((Response<Competition> r) {
-      //  expect((r as Data<Competition>).value, isNotNull);
-      //}));
     });
   });
 }
