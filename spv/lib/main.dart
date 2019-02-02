d66171f7af657f6d93622cadf69b92854ba18d6d
Tim Rijckaert <tim.rijckaert@hotmail.com>
2019-02-02 15:30:35 +0100

    remove abstruse, experimenting with some layout widgets

--- spv/lib/main.dart
+++ spv/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:rxdart/rxdart.dart';
 import 'package:service_layer_spv/spv_service.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Sporza Voetbal'),
     );
   }
 }
@@ -28,8 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   HomeBloc _homeBloc;
   GameDetailBloc _gameDetailBloc;
   UserPreference _userPreferences;
@@ -41,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     var httpClient = http.Client();
-   _cache = CacheImpl();
+    _cache = CacheImpl();
     _network = NetworkImpl(httpClient);
 
     _homeBloc = HomeBloc(_cache, _network);
@@ -57,10 +56,11 @@ class _MyHomePageState extends State<MyHomePage> {
       notLiveRefreshInMilliSeconds: 1000,
     );
 
-    _competitionOverviewBloc = CompetitionOverviewBloc("52", _cache, _network, _userPreferences);
+    _competitionOverviewBloc =
+        CompetitionOverviewBloc("52", _cache, _network, _userPreferences);
   }
 
-  void _incrementCounter() {
+  void _fire() {
     _competitionOverviewBloc.calendar.listen((data) {
       (data as Data<Calendar>)
           .value
@@ -70,7 +70,9 @@ class _MyHomePageState extends State<MyHomePage> {
           .map((match) => (match as Match).id)
           .map((matchId) => [
                 matchId,
-                GameDetailBloc(matchId, _cache, _network, _userPreferences, notLiveRefreshInMilliSeconds: 1000).events
+                GameDetailBloc(matchId, _cache, _network, _userPreferences,
+                        notLiveRefreshInMilliSeconds: 1000)
+                    .events
               ])
           .forEach((pair) {
         (pair.last as Observable<Response<List<Event>>>).listen((eventResp) {
@@ -95,44 +97,17 @@ class _MyHomePageState extends State<MyHomePage> {
       print(resp);
     });
 
-   _homeBloc.news.listen((resp) {
+    _homeBloc.news.listen((resp) {
       print(resp);
     });
 
     _homeBloc.videos.listen((resp) {
       print(resp);
     });
-
-    setState(() {
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+    return Container(color: Colors.green,);
   }
 }
